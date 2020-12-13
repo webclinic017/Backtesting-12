2caba1bb606e4222926f211e57dd8d9ad8237986
Max Mekiska <maxmekiska@gmail.com>
2020-12-13 23:59:48 +0000

    formatting for prophet intake complete

--- backtest.py
+++ backtest.py
@@ -14,8 +14,27 @@ price = stock["Adj Close"]
 d_returns = price.pct_change()
 
 print(d_returns)
-plt.plot(d_returns)
-plt.show()
+#plt.plot(d_returns)
+#plt.show()
+
+d_returns = d_returns.to_frame()
+
+
+# preparation for prophet intake
+d_returns["ds"] = d_returns.index # create ds column from index
+d_returns["y"] = d_returns["Adj Close"] # create y column from adj close price
+
+d_returns.drop('Adj Close', axis=1, inplace=True) # drop original adj close price column
+
+
+d_returns = d_returns.reset_index(drop=True)
+
+
+d_returns = d_returns.set_index('ds')
+
+
+print(d_returns)
+
 
 
 
